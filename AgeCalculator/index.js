55edbe969b35cdb874832c6c4af772bf83e67c19
Pragya Gupta <pragya.gupta012@gmail.com>
2023-06-08 01:44:02 +0530

    added logic

--- AgeCalculator/index.js
+++ AgeCalculator/index.js
@@ -1,4 +1,10 @@
-function calculate(){
-    let userinput = Number(document.getElementById("age").value);
-    
+
+function calculate() {
+    let userinput = document.getElementById("age").value;
+    const date = new Date();
+    let curryear = date.getFullYear();
+    const userdate = new Date(userinput);
+    let birthyear = userdate.getFullYear();
+    let age = curryear - birthyear
+    return document.getElementById("userage").innerHTML=`Your age is ${age}`
 }
